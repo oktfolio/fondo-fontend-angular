75b704f8ed0e0e205493f8eb26f0188d5525492e
oktfolio <oktfolio@gmail.com>
2020-01-06 02:26:21 +0800

    a

--- src/app/gender.pipe.ts
+++ src/app/gender.pipe.ts
@@ -6,27 +6,22 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class GenderPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
-    if (value === 'M') {
-      return '男';
+    switch (value) {
+      case 'M':
+        return '男';
+      case 'F':
+        return '女';
+      case 'U':
+        return '未指定';
+      case 'N':
+        return '不愿透露';
+      case 'O':
+        return '其他';
+      case 'T':
+        return '跨性别';
+      case 'I':
+        return '无性别';
     }
-    if (value === 'F') {
-      return '女';
-    }
-    if (value === 'T') {
-      return '跨性别';
-    }
-    if (value === 'I') {
-      return '无性别';
-    }
-    if (value === 'U') {
-      return '未指定';
-    }
-    if (value === 'N') {
-      return '不愿透露';
-    } else {
-      return '其他';
-    }
-
   }
 
 }
